ce26495443de6daf1c5d544412495b7b94506502
PabloVillapunMartin <pvillapunmartin@gmail.com>
2024-07-31 17:50:09 +0200

    Basic movement

--- Assets/Scripts/Player/PlayerCharacterController.cs
+++ Assets/Scripts/Player/PlayerCharacterController.cs
@@ -1,21 +1,22 @@
 using Godot;
-using System.Diagnostics;
 
 public partial class PlayerCharacterController : CharacterBody2D
 {
-	public AnimationPlayer animationPlayer;
-
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -600.0f;
+	private const float Speed = 300.0f;
+	private const float JumpVelocity = -600.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+    private AnimationPlayer AnimationPlayer = null;
+    private Sprite2D Sprite2D = null;
 
     public override void _EnterTree()
     {
         base._EnterTree();
 
-		animationPlayer = (AnimationPlayer)GetNode("AnimationPlayer");
+        AnimationPlayer = (AnimationPlayer)GetNode("AnimationPlayer");
+		Sprite2D = (Sprite2D)GetNode("Sprite2D");
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -27,7 +28,8 @@ public partial class PlayerCharacterController : CharacterBody2D
 			velocity.Y += gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		bool justJumped = Input.IsActionJustPressed("ui_accept") && IsOnFloor();
+		if (justJumped)
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
@@ -44,5 +46,45 @@ public partial class PlayerCharacterController : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		ManageCharacterAnimation(direction, justJumped);
+	}
+
+	private void ManageCharacterAnimation(Vector2 direction, bool JustJumped)
+	{
+		if (AnimationPlayer == null || Sprite2D == null) return;
+
+		// Sprite flip (right / left)
+		if(direction.X != 0)
+		{
+            Sprite2D.FlipH = direction.X < 0;
+        }
+
+		// Jump
+        if (JustJumped)
+        {
+            AnimationPlayer.Play("player_jump");
+			return;
+        }
+
+        // Animation state
+        if (IsOnFloor())
+		{
+			if(direction.X == 0)
+			{
+				AnimationPlayer.Play("player_idle");
+            }
+			else
+			{
+				AnimationPlayer.Play("player_walk");
+			}
+		}
+		else    // Air
+		{
+			if(Velocity.Y > 0)
+			{
+                AnimationPlayer.Play("player_fall");
+            }
+		}
 	}
 }
