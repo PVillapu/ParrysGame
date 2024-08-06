16d340edcf08077a903158e364722d01ca847b18
PabloVillapunMartin <pvillapunmartin@gmail.com>
2024-08-06 19:08:56 +0200

    Attack areas flipped correctly fix

--- Assets/Scripts/Enemies/EnemyCharacter.cs
+++ Assets/Scripts/Enemies/EnemyCharacter.cs
@@ -6,17 +6,23 @@ public partial class EnemyCharacter : CharacterBody2D
     [ExportGroup("Damage")]
     [Export]
     private int Damage = 0;
-
     [Export]
-    private Area2D[] AttackAreas = new Area2D[0];
+    private Area2D AttackAreas = null;
+    [Export]
+    private Sprite2D CharacterSprite = null;
 
     public override void _EnterTree()
     {
-        foreach (Area2D area in AttackAreas)
-        {
-            if(area == null) continue;
-            area.AreaEntered += OnAttackHitPlayer;
-        }
+        if (AttackAreas == null) return;
+        AttackAreas.AreaEntered += OnAttackHitPlayer;
+    }
+
+    public override void _Process(double delta)
+    {
+        if(CharacterSprite == null) return;
+
+        float X = CharacterSprite.FlipH ? -1f : 1f;
+        AttackAreas.Scale = new Vector2(X, 1);
     }
 
     protected virtual void OnAttackHitPlayer(Area2D area)
